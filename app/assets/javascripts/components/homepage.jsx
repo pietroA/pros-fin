dcf8bc5c48b520d12d92fa72397a09044f3037b6
Pietro Allievi <pietro.allievi@gmail.com>
2019-05-26 18:26:01 +0200

    add menu

--- app/assets/javascripts/components/homepage.jsx
+++ app/assets/javascripts/components/homepage.jsx
@@ -6,9 +6,6 @@ class UserHomePage extends React.Component{
         return(
             <div>
                 <header className="jumbotron text-center">
-                    <nav className="text-right ">
-                        <a className="fa fa-sign-out" rel="nofollow" data-method="delete" href="/users/sign_out"></a>
-                    </nav>
                     <h2><small>Bentornato</small> { this.props.current_user.username}</h2>
                 </header>
                 <UserReports current_user={this.props.current_user} />
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module ProsFin
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.available_locales = [:en, :it]
+    config.i18n.default_locale = :it
     config.time_zone = "Rome"
 
     config.react.addons = true
